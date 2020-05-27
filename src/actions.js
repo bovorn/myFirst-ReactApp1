ec85569f10729877824f17b1debdc74d25f4bd7d
bovorn_C@outlook.com <bovorn_C@outlook.com>
2020-05-27 14:17:37 +0700

    improve accessbillity

--- src/actions.js
+++ src/actions.js
@@ -10,7 +10,8 @@ import {
 
   export const fetchTodos = () => async (dispatch) => {
       dispatch({ type: REQUEST_TODOS_PENDING });
-      fetch('https://jsonplaceholder.typicode.com/todos').then((response) => {return response.json()})
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => { return response.json(); })
         .then(data => dispatch({ type: REQUEST_TODOS_SUCCESS , payload: data}))
         .catch( err => dispatch({ type: REQUEST_TODOS_FAILED , payload: err}));
   }
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,10 +3,11 @@ import React from 'react'
 export default function SearchBox({ onSearchChange }) {
     return (
         <input
+        aria-label="Search Todo"
         className='pa3 ba b--green bg-lightest-blue'
         style={{width: '400px', height: '30px' , maxWidth: '400px'}}
         type='search'
-        placeholder='search..'
+        placeholder='Search...'
         onChange={onSearchChange}
         />
     )
